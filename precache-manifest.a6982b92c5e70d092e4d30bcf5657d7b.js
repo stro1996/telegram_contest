b39612ee03366fc9cb2f1bce0990d6b5aeda0725
Sergey Tarasevich <stro1996@gmail.com>
2019-03-24 16:48:07 +0300

    Updates

---
+++ precache-manifest.a6982b92c5e70d092e4d30bcf5657d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "015fc226047f31ab20b5",
+    "url": "/static/css/main.35c4311d.chunk.css"
+  },
+  {
+    "revision": "015fc226047f31ab20b5",
+    "url": "/static/js/main.607fc29b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "54eb0681770f82dd5713",
+    "url": "/static/js/2.232a318a.chunk.js"
+  },
+  {
+    "revision": "3eeda98739391c72ea35626e9435ec88",
+    "url": "/index.html"
+  }
+];
